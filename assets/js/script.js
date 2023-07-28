bab3a8b8c4ceb9bc3ef8addb820fd5562fa5be12
NavaHub <navaa003@gmail.com>
2023-07-28 01:57:50 -0500

    After AskBCS helper: Readjusted js layout based on advice; added some if statements to attempt on a comparison method; applied many console logs to understand what is going on; still confused on comparison procedure between current hour and div id hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,32 +7,63 @@ $(document).ready(function () {
   ${currDay.format('MMMM/DD/YYYY')}<br>
   ${currDay.format('hh:mm a')}`);
 
-  var placeholderLength = 24;
+  var currHour = dayjs().format('h');
+  // console.log(typeof(currHour), currHour);
+  var businessHours = 9;
 
-  for (var i = 0; i < placeholderLength; i++) {
+  for (var i = 0; i < businessHours; i++) {
     var divHourEl = $('<div>');
-    divHourEl.attr('id', `hour-${(i)}`);
+
+    if (i < 4) {
+      divHourEl.attr('id', `hour-${(i + 9)}`);
+    } else {
+      divHourEl.attr('id', `hour-${(i - 3)}`);
+    }
     divHourEl.addClass('row time-block');
 
-    if(currDay.isSame(dayjs())){
-      divHourEl.addClass('present');
-    }
+    console.log($('.container-lg').find("div[id*='hour-']"));
+    console.log(currHour == $('.container-lg').find("div[id*='hour-']"))
+    console.log(currHour < $('.container-lg').find("div[id*='hour-']"))
+    console.log(currHour > $('.container-lg').find("div[id*='hour-']"))
 
     var hourTextEl = $('<div>');
     hourTextEl.addClass('col-2 col-md-1 hour text-center py-3')
-    if (i < (placeholderLength / 2)) {
-      hourTextEl.text(`${i}AM`);
+    if (i < 3) {
+      hourTextEl.text(`${(i + 9)} AM`);
+    } else if (i == 3) {
+      hourTextEl.text(`${(i + 9)} PM`);
     } else {
-      hourTextEl.text(`${i}PM`);
+      hourTextEl.text(`${(i - 3)} PM`);
     }
-    divHourEl.append(hourTextEl);
+    // console.log(typeof(hourTextEl.text()), hourTextEl.text())
+    // console.log(currHour === hourTextEl.text());
+    // console.log(currHour < hourTextEl.text());
+    // console.log(currHour > hourTextEl.text());
+    // console.log(dayjs().format('h a'), currHour, "Same Hour? " + dayjs().isSame(currHour));
+    // console.log("Before Hour? " + dayjs().isBefore(currHour));
+    // console.log("After Hour? " + dayjs().isAfter(currHour));
 
+    if(currHour === hourTextEl.text()) {
+      divHourEl.removeClass('past');
+      divHourEl.addClass('present');
+      divHourEl.removeClass('future');
+    } else if (currHour < hourTextEl.text()) {
+      divHourEl.addClass('past');
+      divHourEl.removeClass('present');
+      divHourEl.removeClass('future');
+    } else if (currHour > hourTextEl.text()) {
+      divHourEl.removeClass('past');
+      divHourEl.removeClass('present');
+      divHourEl.addClass('future');
+    }
+
+    divHourEl.append(hourTextEl);
     $('.container-lg').append(divHourEl);
   }
-  
-  console.log(currDay);
-  console.log(currDay.isSame(dayjs()));
-  console.log(dayjs());
+
+  // console.log(currDay);
+  // console.log(currDay.isSame(dayjs()));
+  // console.log(dayjs());
 
   $('.saveBtn').on('click', function () {
     console.log(this);
