d289b9c7eb8b48adf238494e7179377c59b8bbf0
NavaHub <navaa003@gmail.com>
2023-07-31 22:44:49 -0500

    Can now remove example code in index.html and reformat the layout; instructor and TAs assistance resulted in using parseInt and different attribute name to acquire a div hour id, applied other elements based on example code from html, correctly applied comparison logic to add and remove classes based on current hour, and proceeding to apply logic based on button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,27 +7,16 @@ $(document).ready(function () {
   ${currDay.format('MMMM/DD/YYYY')}<br>
   ${currDay.format('hh:mm a')}`);
 
-  var currHour = dayjs().format('h');
-  // console.log(typeof(currHour), currHour);
+  var currHour = parseInt(currDay.format('H'));
   var businessHours = 9;
 
   for (var i = 0; i < businessHours; i++) {
     var divHourEl = $('<div>');
-
-    if (i < 4) {
-      divHourEl.attr('id', `hour-${(i + 9)}`);
-    } else {
-      divHourEl.attr('id', `hour-${(i - 3)}`);
-    }
+    divHourEl.attr('data-hour', (i + 9));
     divHourEl.addClass('row time-block');
 
-    console.log($('.container-lg').find("div[id*='hour-']"));
-    console.log(currHour == $('.container-lg').find("div[id*='hour-']"))
-    console.log(currHour < $('.container-lg').find("div[id*='hour-']"))
-    console.log(currHour > $('.container-lg').find("div[id*='hour-']"))
-
     var hourTextEl = $('<div>');
-    hourTextEl.addClass('col-2 col-md-1 hour text-center py-3')
+    hourTextEl.addClass('col-2 col-md-1 hour text-center py-3');
     if (i < 3) {
       hourTextEl.text(`${(i + 9)} AM`);
     } else if (i == 3) {
@@ -35,38 +24,45 @@ $(document).ready(function () {
     } else {
       hourTextEl.text(`${(i - 3)} PM`);
     }
-    // console.log(typeof(hourTextEl.text()), hourTextEl.text())
-    // console.log(currHour === hourTextEl.text());
-    // console.log(currHour < hourTextEl.text());
-    // console.log(currHour > hourTextEl.text());
-    // console.log(dayjs().format('h a'), currHour, "Same Hour? " + dayjs().isSame(currHour));
-    // console.log("Before Hour? " + dayjs().isBefore(currHour));
-    // console.log("After Hour? " + dayjs().isAfter(currHour));
-
-    if(currHour === hourTextEl.text()) {
+    
+    var divDataHour = parseInt(divHourEl.attr('data-hour'));
+    if(currHour === divDataHour) {
       divHourEl.removeClass('past');
       divHourEl.addClass('present');
       divHourEl.removeClass('future');
-    } else if (currHour < hourTextEl.text()) {
-      divHourEl.addClass('past');
-      divHourEl.removeClass('present');
-      divHourEl.removeClass('future');
-    } else if (currHour > hourTextEl.text()) {
+    } else if (currHour < divDataHour) {
       divHourEl.removeClass('past');
       divHourEl.removeClass('present');
       divHourEl.addClass('future');
+    } else if (currHour > divDataHour) {
+      divHourEl.addClass('past');
+      divHourEl.removeClass('present');
+      divHourEl.removeClass('future');
     }
 
+    var textAreaEl = $('<textarea>');
+    textAreaEl.addClass('col-8 col-md-10 description');
+    textAreaEl.attr('rows', 3);
+
+    var btnEl = $('<button>');
+    btnEl.addClass('btn saveBtn col-2 col-md-1');
+    btnEl.attr('aria-label', 'save');
+
+    var iconEl = $('<i>');
+    iconEl.addClass('fas fa-save');
+    iconEl.attr('aria-hidden', 'true');
+    btnEl.append(iconEl);
+
     divHourEl.append(hourTextEl);
+    divHourEl.append(textAreaEl);
+    divHourEl.append(btnEl);
     $('.container-lg').append(divHourEl);
   }
 
-  // console.log(currDay);
-  // console.log(currDay.isSame(dayjs()));
-  // console.log(dayjs());
-
   $('.saveBtn').on('click', function () {
     console.log(this);
+    var selectHour = $(this).parents().attr('data-hour');
+    console.log(selectHour);
   });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -75,15 +71,7 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
